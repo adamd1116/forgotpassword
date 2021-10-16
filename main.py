1d2b499963c95fb76367b74d99335c9919cd793b
adamd1116 <adamdunleavy15@gmail.com>
2021-10-16 20:11:39 +0100

    added main

---
+++ main.py
@@ -0,0 +1,7 @@
+password = 'testpassword123'
+last_password = input("Enter lass password you remembered: ")
+check_password = []
+
+for i in range(len(last_password)):
+    check_password.append(last_password[i])
+
